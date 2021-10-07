825a2d4c0bbe727db676a1d7361b8992a2764606
Kristian <kristian383@gmail.com>
2021-10-07 23:12:35 +0200

    Update website.py

--- models/website.py
+++ models/website.py
@@ -3,7 +3,7 @@ from db import db
 class WebsiteModel(db.Model):
     __tablename__ = "useful_website"
     id = db.Column(db.Integer, primary_key=True)
-    useful_websites_id = db.Column(db.Integer, db.ForeignKey("useful_websites.id"))
+    # useful_websites_id = db.Column(db.Integer, db.ForeignKey("useful_websites.id"))
     name = db.Column(db.String(80))
     link = db.Column(db.String(180))
 
@@ -16,4 +16,20 @@ class WebsiteModel(db.Model):
             return {"name": self.name,
                     "link": self.link,
                     }
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit() 
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
 
