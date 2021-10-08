af77db34a6fbc998355f77665b82b0a7ee0fd286
Kristian <kristian383@gmail.com>
2021-10-08 17:40:55 +0200

    Update website.py

--- resources/website.py
+++ resources/website.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 # from flask_jwt import jwt_required
 from models.website import WebsiteModel
+from models.user import UserModel
 
 
 class Website(Resource):
@@ -15,6 +16,11 @@ class Website(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
 
     def get(self, name):
 
@@ -25,12 +31,20 @@ class Website(Resource):
         return name
 
     def post(self, name):
-        if WebsiteModel.find_by_name(name):
+        data = Website.parser.parse_args()
+        user = UserModel.find_by_username(data["username"])
+
+        if not user:
+            return {"message": "User with that username doesn't exist"}, 400
+
+        user_id = UserModel.find_by_username(data["username"]).json()["id"]
+
+        if WebsiteModel.find_by_name(name,user_id):
             return {'message': "An website with name '{}' already exists.".format(name)}, 400
 
         data = Website.parser.parse_args()
 
-        website = WebsiteModel(data["name"], data["link"])
+        website = WebsiteModel(data["name"], data["link"],user_id)
         # self.insert(artist["name"])
         try:
             website.save_to_db()
@@ -39,10 +53,19 @@ class Website(Resource):
         return {'message': 'Website inserted'}, 201
 
     def delete(self, name):
-        item = WebsiteModel.find_by_name(name)
-        if item:
+        data = Website.parser.parse_args()
+        user = UserModel.find_by_username(data["username"])
+
+        if not user:
+            return {"message": "User with that username doesn't exist"}, 400
+
+        user_id = UserModel.find_by_username(data["username"]).json()["id"]
+        
+        website = WebsiteModel.find_by_name(name,user_id)
+
+        if website:
             try:
-                item.delete_from_db()
+                website.delete_from_db()
             except:
                 return {"message": "An error occured while deleting the website."}, 500
 
