7ed4bb573d50d35606f91ae41a4f68aa334219cc
Kristian <kristian383@gmail.com>
2021-10-09 00:28:29 +0200

    Update song.py

--- resources/song.py
+++ resources/song.py
@@ -48,20 +48,17 @@ class Song(Resource):
         if SongModel.find_by_name(data["name"], user_id):
             return {'message': "An song with name '{}' already exists.".format(data["name"])}, 400
 
-        # return data
-        # check if there is artist already in database
-
-        # data = Song.parser.parse_args()
-        artist = ArtistModel.find_by_name(data["artist"],user_id)
+        artist = ArtistModel.find_by_name(data["artist"], user_id)
 
         if artist is None:
-            #make an artist
-            artist=ArtistModel(data["artist"])
-            pass
+            artist = ArtistModel(data["artist"], user_id)
+            try:
+                artist.save_to_db()
+            except:
+                return {"message": "An error occured inserting an artist."}, 500 
+            #return {"arist": artist.json()}
 
-
-
-        song = SongModel(data["name"], artist.id,user_id)
+        song = SongModel(data["name"], artist.id, user_id)
         try:
             song.save_to_db()
         except:
@@ -71,8 +68,9 @@ class Song(Resource):
         # return {"message":"found artist id '{}'".format(song.json())}
         # find artist id from database to make song object
 
-    def delete(self, name, username):
-        # ovo poboljsati u slucaju ako ima vise pjesama sa istim imenom
+    def delete(self,  username):
+        # omoguciti kaskadno brisanje artista ako nema vise pjesama
+        data = Song.parser.parse_args()
         user = UserModel.find_by_username(username)
 
         if not user:
@@ -80,7 +78,7 @@ class Song(Resource):
 
         user_id = UserModel.find_by_username(username).json()["id"]
 
-        song = SongModel.find_by_name(name, user_id)
+        song = SongModel.find_by_name(data["name"], user_id)
         if song:
             try:
                 song.delete_from_db()
@@ -126,6 +124,7 @@ class UsersSongList(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
+
     def get(self, username):
         #data = UsersSongList.parser.parse_args()
         user = UserModel.find_by_username(username)
