faa0b9db5f7e1da0e92bd3e8c3676c2027213a30
Kristian <kristian383@gmail.com>
2021-10-08 17:40:45 +0200

    Update user_notes.py

--- models/user_notes.py
+++ models/user_notes.py
@@ -5,18 +5,19 @@ class UserNotesModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.String(8000))
     txt_area_height = db.Column(db.Integer)
-    username_id=db.Column(db.Integer, db.ForeignKey("users.id"))
+    username_id=db.Column(db.Integer, db.ForeignKey("user.id"))
     #useful_links= db.relationship("WebsiteModel",lazy="dynamic")      #ovo cak odvojiti mogu
 
-    def __init__(self, username,notes="", txt_area_height=200):
+    def __init__(self, username_id,notes="", txt_area_height=200):
         self.notes = notes
         self.txt_area_height = txt_area_height
-        self.username = username
+        self.username_id = username_id
 
     def json(self):
             return {"notes": self.notes,
                     "txt_area_height": self.txt_area_height,
                     "username_id": self.username_id,
+                    "note_id":self.id
                     #"useful_links": [link.json() for link in self.useful_links.all()]
                     }
 
@@ -28,7 +29,20 @@ class UserNotesModel(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    
+
     @classmethod
-    def find_by_username(cls, name):
-        # "SELECT * FROM items WHERE name=name LIMIT 1"
-        return cls.query.filter_by(name=name).first()
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_userId(cls, user_id):
+        return cls.query.filter_by(username_id=user_id).first()
+
+    # @classmethod
+    # def find_by_noteId(cls, id):
+    #     return cls.query.filter_by(id=id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
