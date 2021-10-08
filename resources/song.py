f6d941118abbc8876e32338c784de7180e0cc4ab
Kristian <kristian383@gmail.com>
2021-10-08 17:40:50 +0200

    Update song.py

--- resources/song.py
+++ resources/song.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from models.song import SongModel
 from models.artist import ArtistModel
+from models.user import UserModel
 
 
 class Song(Resource):
@@ -15,24 +16,52 @@ class Song(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
 
-    def get(self, name):
+    def get(self, name, username):
+        user = UserModel.find_by_username(username)
 
-        song = SongModel.find_by_name(name)
+        if not user:
+            return {"message": "User with that username doesn't exist"}, 400
+
+        user_id = UserModel.find_by_username(username).json()["id"]
+
+        song = SongModel.find_by_name(name, user_id)
 
         if song:
             return song.json()
         return name
 
-    def post(self, name):
-        if SongModel.find_by_name(name):
-            return {'message': "An song with name '{}' already exists.".format(name)}, 400
-
+    def post(self, username):
         data = Song.parser.parse_args()
-        artist_id = ArtistModel.find_by_name(
-            data["artist"]).json()["artist_id"]
+        user = UserModel.find_by_username(username)
 
-        song = SongModel(data["name"], artist_id)
+        if not user:
+            return {"message": "User with that username doesn't exist"}, 400
+
+        user_id = UserModel.find_by_username(username).json()["id"]
+
+        if SongModel.find_by_name(data["name"], user_id):
+            return {'message': "An song with name '{}' already exists.".format(data["name"])}, 400
+
+        # return data
+        # check if there is artist already in database
+
+        # data = Song.parser.parse_args()
+        artist = ArtistModel.find_by_name(data["artist"],user_id)
+
+        if artist is None:
+            #make an artist
+            artist=ArtistModel(data["artist"])
+            pass
+
+
+
+        song = SongModel(data["name"], artist.id,user_id)
         try:
             song.save_to_db()
         except:
@@ -42,9 +71,16 @@ class Song(Resource):
         # return {"message":"found artist id '{}'".format(song.json())}
         # find artist id from database to make song object
 
-    def delete(self, name):
-        #ovo poboljsati u slucaju ako ima vise pjesama sa istim imenom
-        song = SongModel.find_by_name(name)
+    def delete(self, name, username):
+        # ovo poboljsati u slucaju ako ima vise pjesama sa istim imenom
+        user = UserModel.find_by_username(username)
+
+        if not user:
+            return {"message": "User with that username doesn't exist"}, 400
+
+        user_id = UserModel.find_by_username(username).json()["id"]
+
+        song = SongModel.find_by_name(name, user_id)
         if song:
             try:
                 song.delete_from_db()
@@ -53,21 +89,36 @@ class Song(Resource):
 
         return {'message': 'Song deleted'}
 
-    def put(self, name):
+    def put(self, name, username):
         data = Song.parser.parse_args()
-        song = SongModel.find_by_name(name)
+
+        user = UserModel.find_by_username(username)
+
+        if not user:
+            return {"message": "User with that username doesn't exist"}, 400
+
+        user_id = UserModel.find_by_username(username).json()["id"]
+
+        song = SongModel.find_by_name(name, user_id)
 
         if song is None:
-            song = SongModel(name, **data) #ili data["price"], data["store_id"]
+            # ili data["price"], data["store_id"]
+            song = SongModel(name, **data)
         else:
             # song.price = data["price"]
-            #update the song
+            # update the song
             pass
 
         song.save_to_db()
 
         return song.json()
 
+
 class SongList(Resource):
     def get(self):
         return {"songs": [song.json() for song in SongModel.find_all()]}
+
+
+class UsersSongList(Resource):
+    def get(self, user_id):
+        return {"songs": [song.json() for song in SongModel.find_all_user_songs(user_id)]}
