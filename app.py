2ad17c19e68f80c5396b9974603244552ec1f31e
Kristian <kristian383@gmail.com>
2021-10-11 00:11:15 +0200

    Auth

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from collections import UserList
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
-# from flask_jwt import JWT
 from flask_jwt_extended import JWTManager
 
+# from flask_jwt import JWT
+# from security import authenticate, identity
 
-from security import authenticate, identity
-
-from resources.user import UserRegister, User, UserList, UserLogin
+from resources.user import UserRegister, User, UserList, UserLogin , UserLogout
 from resources.artist import Artist, ArtistList, ArtistUserList
 from resources.song import Song, SongList, UsersSongList
 from resources.website import Website, WebsiteList
 from resources.user_notes import UserNotes, UserNotesList
 
 from db import db
+from blocklist import BLOCKLIST
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -33,11 +33,49 @@ def create_tables():
 jwt = JWTManager(app)             # vraca access token
 
 
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+
+    return jti in BLOCKLIST
+
+
+# @jwt.expired_token_loader
+# def expired_token_callback():
+#     return jsonify({
+#         'message': 'The token has expired.',
+#         'error': 'token_expired'
+#     }), 401
+
+# @jwt.invalid_token_loader
+# def invalid_token_callback(error):  
+#     return jsonify({
+#         'message': 'Signature verification failed.',
+#         'error': 'invalid_token'
+#     }), 401
+
+# @jwt.unauthorized_loader
+# def missing_token_callback(error):
+#     return jsonify({
+#         "description": "Request does not contain an access token.",
+#         'error': 'authorization_required'
+#     }), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        "description": "The token has been revoked.",
+        'error': 'token_revoked'
+    }), 401
+
+##ROUTES
+
 api.add_resource(UserRegister, "/register")
 api.add_resource(UserList, "/users")
 api.add_resource(User, "/user/<int:user_id>")
 
 api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
 
 
 # vraca sve userove artiste s pjesmama
--- resources/user.py
+++ resources/user.py
@@ -3,18 +3,20 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
-from werkzeug.security import  check_password_hash
+from werkzeug.security import check_password_hash
 from flask_jwt_extended import (
     create_access_token,
-    create_refresh_token
-    
+    create_refresh_token,
+    jwt_required,
+    get_jwt
+
 )
+
+from blocklist import BLOCKLIST
 
 # jwt_refresh_token_required,
 #     get_jwt_identity,
-#     jwt_required,
 #     get_raw_jwt
-
 
 
 class UserRegister(Resource):
@@ -44,7 +46,7 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data["username"]):
             return {"message": "User with that username already exists"}, 400
 
-        user = UserModel(**data)  
+        user = UserModel(**data)
         user.save_to_db()
 
         return {"message": "User created successfully"}, 201
@@ -66,6 +68,7 @@ class User(Resource):
         user.delete_from_db()
         return {"message": "User deleted"}, 200
 
+
 class UserLogin(Resource):
     parser = reqparse.RequestParser()
 
@@ -85,6 +88,7 @@ class UserLogin(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
+
     def post(self):
         data = UserLogin.parser.parse_args()
 
@@ -95,7 +99,8 @@ class UserLogin(Resource):
         # if user and safe_str_cmp(user.password, data['password']):
         if user and check_password_hash(user.password, data['password']):
             # identity= is what the identity() function did in security.py—now stored in the JWT
-            access_token = create_access_token(identity=user.id, fresh=True)       #identity staviti mail?
+            access_token = create_access_token(
+                identity=user.id, fresh=True)  # identity staviti mail?
             refresh_token = create_refresh_token(user.id)
             return {
                 'access_token': access_token,
@@ -103,6 +108,16 @@ class UserLogin(Resource):
             }, 200
 
         return {"message": "Invalid Credentials!"}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        # jti is "JWT ID", a unique identifier for a JWT.
+        jti = get_jwt()['jti']
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
 
 class UserList(Resource):
     def get(self):
