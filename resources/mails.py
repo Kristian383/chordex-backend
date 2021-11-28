5c68938f93c897f7d9e7eb70f680fdf23f7b5bb3
Kristian <kristian383@gmail.com>
2021-11-28 18:45:21 +0100

    Update mails.py

--- resources/mails.py
+++ resources/mails.py
@@ -93,7 +93,7 @@ class ForgotPassword(Resource):
         msg = EmailMessage()
         msg["Subject"] = "Chordex - Password reset"
         msg["From"] = EMAIL_ADRESS
-        msg["To"] = "kristian383@gmail.com"  # ovo staviti user.email
+        msg["To"] = email  # ovo staviti user.email
         token=user.get_reset_pass_token(user.id) 
         reset_link ="http://localhost:8080/resetpswd?token={0}&email={1}".format(token,user.email)
         # reset_link ="http://localhost:8080/resetpswd?token={}".format(token)
