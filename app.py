4be53266dc51a5dd6b8de5d4fd6729608c4bffca
Kristian <kristian383@gmail.com>
2021-11-28 22:27:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["JWT_SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=12)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=14)
 
 api = Api(app)
 #mail = Mail(app)
