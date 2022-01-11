87718b686c5f29176147a8f5c33d6daac0e57085
Kristian <kristian383@gmail.com>
2022-01-12 00:44:15 +0100

    Update artist.py

--- models/artist.py
+++ models/artist.py
@@ -61,9 +61,9 @@ class ArtistModel(db.Model):
             print("COULDNT SAVE IMG_URL FOR ARTIST")
             return "expired"
         if response.ok:
-            img = response.json()["artists"]["items"][0]["images"][1]["url"]
-            self.img_url = img
             try:
+                img = response.json()["artists"]["items"][0]["images"][1]["url"]
+                self.img_url = img
                 self.save_to_db()
             except:
                 return
