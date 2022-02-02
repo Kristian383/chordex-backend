3406adf4f81f3bd9a4d4d3340f476dec97a8c427
Kristian <kristian383@gmail.com>
2022-02-02 21:23:19 +0100

    Update mails.py

--- resources/mails.py
+++ resources/mails.py
@@ -108,39 +108,6 @@ class ForgotPassword(Resource):
             smtp.send_message(msg)
 
         return {"message": "We received your request. Please check your email."}
-
-# class DeleteAccount(Resource):
-#     parser = reqparse.RequestParser()
-#     parser.add_argument('email',
-#                         type=str,
-#                         required=True,
-#                         help="This field cannot be left blank!"
-#                         )
-#     def post(self):
-#         data = ForgotPassword.parser.parse_args()
-#         email = data["email"]
-#         user = UserModel.find_by_email(email)
-#         if not user:
-#             return {"message": "User with that email doesnt exist."}, 400
-
-#         msg = EmailMessage()
-#         msg["Subject"] = "Chordex - Delete account"
-#         msg["From"] = EMAIL_ADRESS
-#         msg["To"] = user.email  
-#         # token=user.get_reset_pass_token(user.id) 
-#         delete_link ="https://chordex.net/deleteacc?token={0}&email={1}".format(token,user.email)
-#         # delete_link ="http://localhost:8080/resetpswd?token={}".format(token)
-#         msg_content = "Someone requested deleting the following account:\n\nhttps://chordex.net\n\nEmail: {0}\n\nIf this was a mistake, just ignore this email and nothing will happen.\n\nTo delete your account, visit the following address:\n\nLink is valid for 30 minutes.\n\n {1}".format(
-#             user.email, delete_link)
-
-#         msg.set_content(msg_content)
-
-#         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-#             smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
-
-#             smtp.send_message(msg)
-
-#         return {"message": "We received your request. Please check your email."}
     
 # ako zelim svima poslati mail
 # contacts=["test@test.com","test2@gmail.com"]
