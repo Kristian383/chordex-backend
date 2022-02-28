1c1b307d44399757cf0d7dcaa84be000cbf6445e
Kristian <kristian383@gmail.com>
2022-02-28 12:43:55 +0100

    Songs limit = 40

--- resources/song.py
+++ resources/song.py
@@ -233,7 +233,7 @@ class Song(Resource):
         if not user:
             return {"message": "User with that email doesn't exist"}, 400
 
-        if user.userHasBenefits() == False and user.count_all_user_songs() >= 30:
+        if user.userHasBenefits() == False and user.count_all_user_songs() >= 40:
             return {"message": "Limit of songs exceeded"}, 400
 
         artist = ArtistModel.find_by_name(data["artist"], user.id)
